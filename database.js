4c9b8d1b592f8e10f797fcb151ad570ed1d66514
Jakalgit <72514800+Jakalgit@users.noreply.github.com>
2022-09-09 18:39:06 +0300

    Update database.js

--- database.js
+++ database.js
@@ -1,18 +1,18 @@
 const {Sequelize} = require("sequelize")
 
 module.exports = new Sequelize(
-    process.env.DB_NAME, // Название БД
-    process.env.DB_USER, // Пользователь
-    process.env.DB_PASSWORD, // Пароль
+    'da3r9dbh5p33u6', // Название БД
+    'jflkbjlagssohy', // Пользователь
+    '1baffb55afce508c55bd669d43b8c2fe51be2f8163c629a8a14cfe597f0b6bc7', // Пароль
     {
         dialect: 'postgres',
-        host: process.env.DB_HOST,
+        host: 'ec2-52-30-159-47.eu-west-1.compute.amazonaws.com',
         dialectOptions: {
             ssl: {
                 require: true,
                 rejectUnauthorized: false
             }
         },
-        port: process.env.DB_PORT,
+        port: '5432,
     },
 )
